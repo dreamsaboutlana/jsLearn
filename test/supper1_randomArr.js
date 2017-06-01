5873f9684cf839cd4dab774a3f880af74ec2ebe8
dreamsaboutlana <Sveta@worker.local>
2017-06-01 21:06:43 +0300

    Function random

--- test/supper1_randomArr.js
+++ test/supper1_randomArr.js
@@ -10,6 +10,7 @@
  [0,16,24,32] => 8
   
  */
+ 
 let arr = [1, 3, 5, 9];
 
 function random(arr) {
@@ -19,20 +20,26 @@ function random(arr) {
   for (var i = 0; i < arr.length - 1; i++) {
 
     if ((step ^ 0) === step && (arr[i + 1] - arr[i]) !== step) {
+
       let mistNum = arr[i] + step;
+
       arr.splice(i + 1, 0, mistNum);
+      return arr;
     } else {
+
       let newStep = arr[i + 1] - arr[i];
-      arr.unshift(arr[i] - newStep);
+      let lostNum = arr[0] - newStep;
+       arr.splice(0, 0,lostNum);
+      return arr;
     }
 
   }
 
-  return arr;
+  
 
 }
 
 // console.log(random([1, 3, 5, 9]));
 // console.log(random([0, 8, 16, 32]));
-console.log(random([0, 16, 24, 32]));
+// console.log(random([0, 16, 24, 32]));
 // console.log(random([4, 6, 8, 10]));
