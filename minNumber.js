c706e03de49422d42263b173f1fc3cc88bb212c8
dreamsaboutlana <Sveta@worker.local>
2017-06-12 10:51:10 +0300

    Del

--- minNumber.js
+++
@@ -1,9 +0,0 @@
-function min(a, b) {
-  if (a > b) {
-    return b;
-  }
-  return a;
-}
-console.log(min(4, 7)); //4
-console.log(min(0, -17));// -17
-console.log(min(-4, -7));//-7
--- obj_construktor.js
+++
@@ -1,8 +0,0 @@
-function Person(name, gender, age) {
-	this.name = name;
-	this.age = age;
-	this.gender = gender;
-}
-
-var man = new Person('Peter');
-var woman = new Person('Sue');
